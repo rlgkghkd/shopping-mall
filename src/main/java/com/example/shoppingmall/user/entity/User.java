f0310f1cc8947f923d224ba513ccf4b5ad99bf35
kkio22 <nahyeon2252@gmail.com>
2025-05-16 17:37:05 +0900

    feat : User Entity 생성

--- src/main/java/com/example/shoppingmall/user/entity/User.java
+++ src/main/java/com/example/shoppingmall/user/entity/User.java
@@ -1,6 +1,13 @@
 package com.example.shoppingmall.user.entity;
 
+import java.time.LocalDateTime;
+
+import com.example.shoppingmall.user.enums.UserRole;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,5 +19,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class User {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(nullable = false)
+	private String username;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	//@Enumerated(value = EnumType.STRING)
+	//@Convert(converter = UserRoleConverter.class) => autoApply = ture가 자동으로 해줌
+	private UserRole userRole;
+	@Column
+	private LocalDateTime createdAt;
+	@Column
+	private Boolean isDeleted;
+
 }

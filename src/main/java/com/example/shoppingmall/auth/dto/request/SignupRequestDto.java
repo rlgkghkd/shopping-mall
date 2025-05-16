78d39591fbda05ad1801c53ba13220403f5a6c38
kkio22 <nahyeon2252@gmail.com>
2025-05-16 20:06:13 +0900

    feat : auth SignupRequestDto 생성

---
+++ src/main/java/com/example/shoppingmall/auth/dto/request/SignupRequestDto.java
@@ -0,0 +1,23 @@
+package com.example.shoppingmall.auth.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignupRequestDto {
+	@NotBlank(message = "이름을 입력해주세요")
+	private String userName;
+	@NotBlank(message = "이메일을 입력해주세요")
+	private String email;
+	@NotBlank(message = "비밀번호를 입력해주세요")
+	@Pattern(
+		regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$",
+		message = "비밀번호는 영문자, 숫자, 특수문자를 포함해 8~20자여야 합니다."
+	)
+	private String password;
+}

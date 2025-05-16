c895554dfdbbd03e0f20899c7aea6833d6f9ebf1
rlgkghkd <rlgkghkd@gmail.com>
2025-05-16 21:23:38 +0900

    fix: joincolumn 명시

--- src/main/java/com/example/shoppingmall/like/entity/Like.java
+++ src/main/java/com/example/shoppingmall/like/entity/Like.java
@@ -12,11 +12,12 @@ import com.example.shoppingmall.user.entity.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,17 +31,19 @@ public class Like {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JoinColumn(name = "comment_id")
 	private Comment comment;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JoinColumn(name = "item_id")
 	private Item item;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@OnDelete(action = OnDeleteAction.CASCADE)
-	@NotNull
+	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 
 	@CreatedDate

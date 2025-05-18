ff66a127a38d7dad9bc5aaf1fb7c3bed0983941d
kkio22 <nahyeon2252@gmail.com>
2025-05-18 20:07:34 +0900

    feat : signUp, login, logout 기능 추가

--- src/main/java/com/example/shoppingmall/auth/controller/AuthController.java
+++ src/main/java/com/example/shoppingmall/auth/controller/AuthController.java
@@ -1,10 +1,21 @@
 package com.example.shoppingmall.auth.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.shoppingmall.auth.dto.request.LoginRequestDto;
+import com.example.shoppingmall.auth.dto.request.SignUpRequestDto;
+import com.example.shoppingmall.auth.dto.response.LoginResponseDto;
+import com.example.shoppingmall.auth.dto.response.SignUpResponseDto;
 import com.example.shoppingmall.auth.service.AuthService;
+import com.example.shoppingmall.common.JwtUtil;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -12,4 +23,24 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 	private final AuthService authService;
+	private final JwtUtil jwtUtil;
+
+	@PostMapping("/signup")
+	public ResponseEntity<SignUpResponseDto> signUp(@Valid @RequestBody SignUpRequestDto signupRequestDto) {
+		SignUpResponseDto signUpResponseDto = authService.signUp(signupRequestDto);
+		return new ResponseEntity<>(signUpResponseDto, HttpStatus.CREATED);
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+		LoginResponseDto loginResponseDto = authService.login(loginRequestDto);
+		return new ResponseEntity<>(loginResponseDto, HttpStatus.OK);
+	}
+
+	@PostMapping("/logout")
+	public ResponseEntity<String> logout(HttpServletRequest request) {
+		authService.logout(request);
+		return ResponseEntity.ok("로그아웃 되었습니다.");
+	}
+
 }

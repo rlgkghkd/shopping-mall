2d9a97ef970c6f480fe8ea17693c7f416e42829f
kkio22 <nahyeon2252@gmail.com>
2025-05-18 17:32:49 +0900

    feat : Spring Security 기능 추가

---
+++ src/main/java/com/example/shoppingmall/common/JwtUtil.java
@@ -0,0 +1,71 @@
+package com.example.shoppingmall.common;
+
+import java.rmi.ServerException;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.Value;
+
+@Component
+public class JwtUtil {
+
+	private static final String BEARER_PREFIX = "Bearer ";
+	private static final long TOKEN_TIME = 60 * 60 * 1000L;
+
+	@Value("${jwt.secret}")
+	private String secretKey;
+	private Key key;
+	private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+	@PostConstruct
+	public void init() {
+		byte[] bytes = Base64.getDecoder().decode(secretKey);
+		key = Keys.hmacShaKeyFor(bytes);
+	}
+
+	public String createToken(Long userId, String userName, String email, String userRole) {
+		Date date = new Date();
+
+		return BEARER_PREFIX + Jwts.builder() //jwt을 이렇게 만들겠다 하는 매서드
+			.setSubject(String.valueOf(userId))
+			.claim("userRole", userRole)
+			.setExpiration(new Date(date.getTime() + TOKEN_TIME))
+			.setIssuedAt(date)
+			.signWith(key, signatureAlgorithm)
+			.compact();
+	}
+
+	public String subStringToken(String tokenValue) throws ServerException {
+		if (StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
+			return tokenValue.substring(7);
+		}
+		throw new ServerException("NOT_FOUND_TOKEN");
+	}
+
+	public Claims extractClaim(String token) {
+		return Jwts.parserBuilder()
+			.setSigningKey(key)
+			.build()
+			.parseClaimsJwt(token)
+			.getBody();
+	}
+
+	public boolean validationToken(String token) {
+		try {
+			extractClaim(token);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+}
---
+++ src/main/java/com/example/shoppingmall/common/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.example.shoppingmall.common;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+//@Slf4j 어노테이션을 왜 쓰는지 앞으로 생각하고 쓰자
+@EnableWebSecurity //security 사용할 때 필요한 친구
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final SecurityFilter securityFilter;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable)
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests(
+				auth -> auth
+					.requestMatchers("/api/auth/**")
+					.permitAll()
+					.requestMatchers(HttpMethod.GET, "/api/users")
+					.hasRole("ADMIN")
+					.anyRequest()
+					.authenticated()
+			)
+			.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class); //이걸로 filter 등록함
+
+		return http.build();
+	}
+	/*
+	JWT + Security 구동 방식
+	=> 처음에 로그인해서 들어오면 securityConfig에 와서 addFilterBefore에 적혀있는 securityFilter로 가서 token이 있는지 확인하고,
+	있으면 파싱해서 claim을 얻는다 그리고나서
+	UsernamePasswordAuthenticationFilter.class가 실행되고, 폼 로그인 요청을 처리한다.
+	그리고 나서 이 securityConfig 클래스에 설정해 놓은 url을 허락할지 말지를 결정해서 못 들어가는
+	role이면 에러를 반환하고 맞으면 controller로 들어간다.
+	그리고 처음 회원가입하는 친구는 틀러와서 securityfilter로 가면 토큰 값이 없기 때문에
+	그냥 filter를 뺘져 나와서 securityConfig로 오는데 이때 무조건 허락하는 url로 적어놓았기 때문에 controller로 들어갈 수 있다.
+
+	=> 내용 다시 정리
+	 */
+
+}

d6aedad9758134b95b3bd7b95f3387371e54dad4
kkio22 <nahyeon2252@gmail.com>
2025-05-18 17:36:17 +0900

    feat : password 암호화 기능 추가

--- src/main/java/com/example/shoppingmall/common/SecurityConfig.java
+++ src/main/java/com/example/shoppingmall/common/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -50,4 +52,8 @@ public class SecurityConfig {
 	=> 내용 다시 정리
 	 */
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
